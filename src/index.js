840555fbcb6944dbbea496b83525d6e4738260ce
vano <kolesen.ivan@gmail.com>
2018-11-12 16:32:19 +0200

    feat: add todos rendering

--- src/index.js
+++ src/index.js
@@ -2,18 +2,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import scss from "../styles/style.scss";
 import $ from "jquery";
 
-$(document).ready(() => {
-  const toDoList = JSON.parse(localStorage.getItem("toDoList")) || {};
+const toDoList = JSON.parse(localStorage.getItem("toDoList")) || {};
 
-  $('#add-btn').click(() => {
+$(document).ready(() => {
+
+  renderToDos();
+
+
+  $('#add-btn').click((e) => {
+    const title = $('#input-title').val();
+    const category = $('#input-category').val();
     const toDoId = Math.random().toString();
-    const newToDo = {
-      title: $('#input-title').val(),
-      category: $('#input-category').val(),
-      issCompleted: false
+
+    console.log(title, category);
+
+    if (title && category) {
+      const newToDo = {
+        title: title,
+        category: category,
+        issCompleted: false
+      };
+      Object.assign(toDoList, {[toDoId]: newToDo});
+      localStorage.setItem("toDoList", JSON.stringify(toDoList));
     };
-    Object.assign(toDoList, {[toDoId]: newToDo});
-    localStorage.setItem("toDoList", JSON.stringify(toDoList));
   });
 
 });
+
+const renderToDos = () => {
+  for (let key in toDoList) {
+    $('.todo-items').append(
+      `<div class="todo-item">
+      <div class="todo-item__info ">
+        <div class="info-name">${toDoList[key].title}</div>
+        <div class="info-category">${toDoList[key].category}</div>
+      </div>
+      <div class="todo-item__complete-btn">✓</div>
+      <div class="todo-item__delete-btn">✗</div>
+    </div>`
+    );
+  }
+};
