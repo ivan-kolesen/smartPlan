b20e69c599e970eea073173fb03eea737808994f
vano <kolesen.ivan@gmail.com>
2018-11-12 14:58:59 +0200

    feat: realize the opportunity to add to do

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import scss from "../styles/style.scss";
+import $ from "jquery";
+
+$(document).ready(() => {
+  const toDoList = JSON.parse(localStorage.getItem("toDoList")) || {};
+
+  $('#add-btn').click(() => {
+    const toDoId = Math.random().toString();
+    const newToDo = {
+      title: $('#input-title').val(),
+      category: $('#input-category').val(),
+      issCompleted: false
+    };
+    Object.assign(toDoList, {[toDoId]: newToDo});
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));
+  });
+
+});
